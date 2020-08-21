8d75ea85a7445995b95c5830fbe15772207a070b
Jiin <42488309+zeen263@users.noreply.github.com>
2020-08-21 21:04:45 +0900

    20.08.21.

---
+++ 1929) 소수 구하기.py
@@ -0,0 +1,16 @@
+import sys
+
+M,N = map(int,sys.stdin.readline().split())
+isprime = [1 for x in range(N+1)]
+isprime[1] = 0
+
+for i in range(2, int(N**0.5)+1):
+    if isprime[i] == 0: continue
+
+    for j in range(i*i, N+1, i):
+        isprime[j] = 0
+
+for i in range(M, len(isprime)):
+    if isprime[i]: print(i)
+
+
