6d97082f41b244c3a787382e9f092d76c903dddf
J <42488309+zeen263@users.noreply.github.com>
2020-11-26 19:46:33 +0900

    20.11.26.

--- leet 378) Kth Smallest Element in a Sorted Matrix.py
+++ leet 378) Kth Smallest Element in a Sorted Matrix.py
@@ -6,10 +6,11 @@ matrix =
    
    k = 5인 경우 5번째로 작은 수인 11 반환
 '''
-
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         numlist = sum(matrix, [])
+        # itertools import해서  numlist = list(itertools.chain(*matrix)) 처럼 쓸 수도 있다.
+        # itertools 쓰는 게 정석인 듯
+              
         numlist.sort()
-        
         return numlist[k-1]
