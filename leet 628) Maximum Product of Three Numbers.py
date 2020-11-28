7beda841c8e988adbdc8d0f9b1eba359da390552
J <42488309+zeen263@users.noreply.github.com>
2020-11-28 16:17:21 +0900

    20.11.28.

--- leet 628) Maximum Product of Three Numbers.py
+++ leet 628) Maximum Product of Three Numbers.py
@@ -1,7 +1,6 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         # 정렬하고 나면 최대값이 가능한 조합 : (마이너스두개 플러스한개) 또는 (플러스세개)
-        
         nums = sorted(nums)   
         res1 = nums[0]*nums[1]*nums[-1]
         res2 = nums[-1]*nums[-2]*nums[-3]
