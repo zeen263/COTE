270c797f768de84f66834a459f37a52044a73dcb
zeen263 <zeen263@naver.com>
2021-08-23 20:10:00 +0900

    21.07.29.

---
+++ 6603) 로또 (pythonic).py
@@ -0,0 +1,18 @@
+'''
+itertools는 신이다
+근데 6중 for문 버전이 더 빠르다 어째서
+'''
+
+import sys, itertools
+
+while True:
+    s = sys.stdin.readline()
+    if s[0]=="0": break
+
+    pool = list(map(int, s.split()))
+    k = pool.pop(0)
+
+    res = itertools.combinations(pool, 6)
+    for combi in res:
+        print(' '.join(map(str, combi)))
+    print()
